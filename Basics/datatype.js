0c66ac06ca10ca336f30dc8bb6274cd88637a84c
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2023-12-13 11:53:01 +0000

    Discussion on datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict " // treat all JS code in newer version
+
+// alert (3+3) we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Pranav")
+
+let name = "Pranav"
+let age = 20
+let isLoggedin = "True"
+let state;
+
+/*
+Primitive Datatypes
+
+name = string // string should be in "" quotes
+isLoggedin = boolean // it returns true of false value
+null = standalone value (object)
+undefined =  means value is not assigned
+symbol = used to find uniqueness, used in reactjs
+
+*/
+
+console.log(typeof undefined) // returns undefined value
+console.log(typeof null) // returns null value
