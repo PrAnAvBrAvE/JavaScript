56c5890e4c901756018542c0d5583e89f40f9cdb
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2023-12-14 11:15:43 +0000

    Conversion of Datatypes

---
+++ Basics/conversionofdatatypes.js
@@ -0,0 +1,30 @@
+let number = "123"
+// console.log(typeof number);
+
+let valuenumber = Number (number)
+// console.log(typeof valuenumber);
+// console.log(valuenumber);
+
+// 123 = 123
+// 123abc = NaN
+// null = 0
+// "" = 0
+// true = 1 & false = 0
+// undefined = NaN
+
+let bool = undefined
+
+let TrueorFalse = Boolean(bool) 
+// console.log(TrueorFalse);
+
+// 1 = true & 0 = false
+// "Pranav" = True
+// "" = false
+// null = false
+// undefined = false
+
+let some = 22
+
+let somenumber = String(some)
+console.log(somenumber);
+console.log(typeof somenumber);
