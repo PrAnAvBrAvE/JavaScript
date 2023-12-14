9fb2d0b8ee911e68ac9c58d178a99719aef17e68
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2023-12-14 11:50:45 +0000

    Operations of JS

---
+++ Basics/4_operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+/* console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2); */
+
+let str1 = "Hello "
+let str2 = "Pranav"
+let str3 = str1 + str2
+// console.log(str3);
+
+// complex operations
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // if string is first then all others treated as string. (postfix)
+console.log(1 + 2 + "2"); // if string is last then all others are not treated as string. (prefix)*/
+
+/* console.log(+true); // 1
+console.log(+false); // 0
+console.log(+""); // 0 */
+
+let gamecounter = 100
+++gamecounter // (prefix) & gamecounter++ (postfix)
+console.log(gamecounter);
