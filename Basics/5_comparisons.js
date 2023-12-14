00e898b563dc2df136f35af74993955ef3930a51
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2023-12-14 12:10:45 +0000

    Comparisons of JS

---
+++ Basics/5_comparisons.js
@@ -0,0 +1,24 @@
+/* console.log(2>1) true
+console.log(2>=1) true
+console.log(2==1) false
+console.log(2<1) false
+console.log(2<=1) false */
+
+// console.log("2" > 1 + 3); false 
+// console.log(2 > "1") true
+
+/* console.log(null > 0); // false
+console.log(null < 0); // false
+console.log(null <= 0); // true
+console.log(null >= 0); // true (<, >, <=, >=) comparisons operations. Comparisons convert null to a number. Treating it as 0.
+console.log(null == 0); // false (==) equality check operations. */
+
+// comparisons and equality check are different 
+
+/* console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+console.log(undefined == 0); // false */
+
+console.log(2 === 2); // true(its value is same as well as its datatype is also same). === it also checks values as well as its datatypes strictly. 
