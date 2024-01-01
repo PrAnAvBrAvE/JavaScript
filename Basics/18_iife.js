375f22735e57c20adbbf086f0ad91093619758ee
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-01-01 06:22:59 +0000

    Arrows in js

--- Basics/18_iife.js
+++ Basics/18_iife.js
@@ -1,11 +1,17 @@
 // Immediately Invoked Function Expressions (IIFE)
+// IIFE is used to control the pollution of global scope.
 
+(function fun()
+{
+    // Named IIFE
+    console.log(`DB CONNECTED` ); // here we simply print the string
 
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
-})();
+})(); // here first parenthesis is for function definition and second parenthesis is for function
+// execution. Always remember that if we are declaring two iffe's we have to give semicolon after
+// first iife. without it, it will give error. Here we explicitly give semicolon to end the code.
 
-( (name) => {
+( (name) => 
+{
+    // Simple/Unnamed IIFE
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+})('Pranav') // here we declare a function in format of array function.
