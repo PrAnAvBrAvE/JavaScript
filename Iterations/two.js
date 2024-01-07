864fc88fed73ba8bbccd13deafdd8b6596268448
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-01-07 07:18:23 +0000

    While & Do while loop in js

---
+++ Iterations/two.js
@@ -0,0 +1,29 @@
+// while loop
+
+let index = 0
+while (index <= 10) // here we give the condition and according to that condition loop will execute.
+{
+    // console.log(`Value of index is ${index}`); // here we simply print the value of index. 
+    index += 2 // here we increment the index value by 2.
+}
+
+let myArray = ['flash', "batman", "superman"] // here we create an array.
+
+let arr = 0
+
+while (arr < myArray.length) 
+{
+    // console.log(`Value is ${myArray[arr]}`); here we simply print the array.
+    arr += 1 // here we increment the array by 1.
+}
+
+// do while loop
+
+let score = 1
+
+do 
+{
+    console.log(`Score is ${score}`); // here print the value of score.
+    score += 1 // here we increment the value of score.
+} while (score <= 10); // here we give the condition and according to that condition loop will 
+// execute.
