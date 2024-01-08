cddf54164e275034a78a1e41e5ea173675673168
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-01-08 07:26:20 +0000

    Loops Discussion in JS

---
+++ Iterations/for_in_loop.js
@@ -0,0 +1,32 @@
+// for in loop
+
+const myObject = 
+{
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+} // here we create an object.
+
+for (const key in myObject) // here we use for in loop.
+{
+    console.log(`${key} shortcut is for ${myObject[key]}`); // here we print the object.
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) 
+{
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) 
+{
+    // console.log(key);
+}
