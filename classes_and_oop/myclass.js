c71603246316f683e62b552f2f71829827009f67
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-07-08 14:56:59 +0000

    Class in JS

--- classes_and_oop/myclass.js
+++ classes_and_oop/myclass.js
@@ -1,6 +1,6 @@
 // // ES6
 
-// class User {
+// class User { 
 //     constructor(username, email, password){
 //         this.username = username;
 //         this.email = email;
@@ -13,30 +13,32 @@
 //     changeUsername(){
 //         return `${this.username.toUpperCase()}`
 //     }
-
 // }
 
-// const chai = new User("chai", "chai@gmail.com", "123")
+// const chai = new User("pranav", "pranav@gmail.com", "123")
 
 // console.log(chai.encryptPassword());
 // console.log(chai.changeUsername());
 
 // behind the scene
 
-function User(username, email, password){
+// if class functionality is not available then we can simply create user and its properties using
+// function.
+
+function User(username, email, password){ // here we call a function
     this.username = username;
     this.email = email;
     this.password = password
 }
 
-User.prototype.encryptPassword = function(){
+User.prototype.encryptPassword = function(){ // here we created a function manually
     return `${this.password}abc`
 }
 User.prototype.changeUsername = function(){
     return `${this.username.toUpperCase()}`
 }
 
-const tea = new User("tea", "tea@gmail.com", "123")
+const tea = new User("sujay", "sujay@gmail.com", "123")
 
 console.log(tea.encryptPassword());
 console.log(tea.changeUsername());
